b2083809d52a642fff540f968fb728fbc8853aac
sruthi sai <vallalarishwanth@gmail.com>
2024-09-11 13:45:06 +0530

    ADD default page

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,18 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
     @GetMapping("/")
-    public String hello() {
-        return "application is at /demo.html";
+    public ModelAndView serveHtml() {
+        return new ModelAndView("demo");
     }
 }
